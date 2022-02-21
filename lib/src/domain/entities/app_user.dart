c8322f71faf59427b963eea703a889b450d26f7c
Felipe Abreu <felipeabreu.rj@gmail.com>
2022-02-21 08:33:40 -0300

    add app user entity

---
+++ lib/src/domain/entities/app_user.dart
@@ -0,0 +1,38 @@
+import 'package:equatable/equatable.dart';
+
+class AppUser extends Equatable {
+  const AppUser({
+    required this.userId,
+    required this.fullName,
+    required this.email,
+    required this.password,
+    required this.imageUrl,
+    required this.role,
+  });
+
+  factory AppUser.empty() {
+    return const AppUser(
+      userId: '',
+      fullName: '',
+      email: '',
+      password: '',
+      imageUrl: '',
+      role: '',
+    );
+  }
+
+  final String userId;
+  final String fullName;
+  final String email;
+  final String password;
+  final String imageUrl;
+  final String role;
+
+  @override
+  List<Object> get props {
+    return [userId, fullName, email, password, imageUrl, role];
+  }
+
+  @override
+  bool get stringify => true;
+}
